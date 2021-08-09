6995c02eb482b63255e626dff41f09d34511b55e
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 23:44:10 +0530

    [sagarika]: Repeating code Till it reaches winning point

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -6,29 +6,29 @@ public class SnakeAndLadder {
 	public static void main(String[] args){
 		int PositionOfPlayer = 0;
 	
-		Random rand = new Random();
-		int RolledDice = (rand.nextInt(6)+1);
-			
-		System.out.println("The Number on the dice is: " +RolledDice);
-		
-		Random randOption = new Random();
-		int Option = (randOption.nextInt(3));
-			
-		if (Option == 1) {
-			PositionOfPlayer = RolledDice + PositionOfPlayer;
+		while (PositionOfPlayer<100) {
+			Random rand = new Random();
+			int RolledDice = (rand.nextInt(6)+1);
 				
-			System.out.println("Player got Ladder, position of player is:" +PositionOfPlayer);
-			System.out.println();
-		}
-		else if (Option == 2) {
-			PositionOfPlayer = Math.abs(RolledDice - PositionOfPlayer);
-				 
-			System.out.println("Player got Snake, position of player is: " +PositionOfPlayer);
-			System.out.println();
-		}
-		else {
-			System.out.println("Player got No Play, position of player is: " +PositionOfPlayer);
-			System.out.println();
+			System.out.println("The Number on the dice is: " +RolledDice);
+			
+			Random randOption = new Random();
+			int Option = (randOption.nextInt(3));
+				
+			if (Option == 1) {
+				PositionOfPlayer = RolledDice + PositionOfPlayer;
+				System.out.println("Player got Ladder, position of player is:" +PositionOfPlayer);
+				System.out.println();
+			}
+			else if (Option == 2) {
+					PositionOfPlayer = Math.abs(RolledDice - PositionOfPlayer);
+					System.out.println("Player got Snake, position of player is: " +PositionOfPlayer);
+					System.out.println();	
+			}
+			else {
+				System.out.println("Player got No Play, position of player is: " +PositionOfPlayer);
+				System.out.println();				
+			}
 		}
 	}
 }
