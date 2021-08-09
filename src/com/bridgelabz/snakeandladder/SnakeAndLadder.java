9b7708ca93f2b4930531d79b407df1a39497f2ad
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 21:13:05 +0530

    [sagarika]: Added the roll dice feature

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,10 +1,19 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
-	private void psvm() {
+	public static void main(String[] args){
 		int Snake;
 		int Ladder;
 		int PositionOfPlayer = 0;
+		
+		Random rand = new Random();
+		int RolledDice = (rand.nextInt(6)+1);
+		
+		System.out.println("The Number on the dice is: " +RolledDice);
+		
+		
 
 	}
 
