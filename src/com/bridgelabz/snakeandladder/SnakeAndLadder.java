2d5cdecc7ba2b08a1fc0a3ccca245e2206f954eb
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 22:50:52 +0530

    [sagarika]: Player options are added

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,17 +4,33 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	public static void main(String[] args){
-		int Snake;
-		int Ladder;
 		int PositionOfPlayer = 0;
-		
+	
 		Random rand = new Random();
 		int RolledDice = (rand.nextInt(6)+1);
-		
+			
 		System.out.println("The Number on the dice is: " +RolledDice);
 		
-		
-
+		Random randOption = new Random();
+		int Option = (randOption.nextInt(3));
+			
+		if (Option == 1) {
+			PositionOfPlayer = RolledDice + PositionOfPlayer;
+				
+			System.out.println("Player got Ladder, position of player is:" +PositionOfPlayer);
+			System.out.println();
+		}
+		else if (Option == 2) {
+			PositionOfPlayer = Math.abs(RolledDice - PositionOfPlayer);
+				 
+			System.out.println("Player got Snake, position of player is: " +PositionOfPlayer);
+			System.out.println();
+		}
+		else {
+			System.out.println("Player got No Play, position of player is: " +PositionOfPlayer);
+			System.out.println();
+		}
 	}
-
 }
+
+
