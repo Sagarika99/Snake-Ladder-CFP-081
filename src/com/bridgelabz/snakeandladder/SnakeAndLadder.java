1b90bd1eaca77058575ca82ab1763a623e2dda88
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-10 10:20:52 +0530

    [sagarika]: Player gets to exact Winning Position feature added

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class SnakeAndLadder {
 	public static void main(String[] args){
 		int PositionOfPlayer = 0;
-	
-		while (PositionOfPlayer<100) {
+			
+		 do {
 			Random rand = new Random();
 			int RolledDice = (rand.nextInt(6)+1);
 				
@@ -16,9 +16,14 @@ public class SnakeAndLadder {
 			int Option = (randOption.nextInt(3));
 				
 			if (Option == 1) {
-				PositionOfPlayer = RolledDice + PositionOfPlayer;
-				System.out.println("Player got Ladder, position of player is:" +PositionOfPlayer);
-				System.out.println();
+				if (PositionOfPlayer + RolledDice <= 100) {
+					PositionOfPlayer = PositionOfPlayer + RolledDice;
+					System.out.println("Player got Ladder, position of player is:" +PositionOfPlayer);
+					System.out.println();
+				 }
+				 else {
+					PositionOfPlayer = PositionOfPlayer;
+				 }
 			}
 			else if (Option == 2) {
 				PositionOfPlayer = Math.abs(RolledDice - PositionOfPlayer);
@@ -29,8 +34,7 @@ public class SnakeAndLadder {
 				System.out.println("Player got No Play, position of player is: " +PositionOfPlayer);
 				System.out.println();				
 			}
-		}
+		}while (PositionOfPlayer < 100);
+	
 	}
 }
-
-
